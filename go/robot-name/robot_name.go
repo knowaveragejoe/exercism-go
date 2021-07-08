afe8ada1ac92006cc8236413a77873cc935ab5d1
knowaveragejoe <25515709+knowaveragejoe@users.noreply.github.com>
2021-07-08 16:45:02 -0400

    Completing the robot_name exercise on desktop

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -15,23 +15,38 @@ var (
 	letters    = [27]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 )
 
+// Retrieve the Robot's name if already set, otherwise generate, set and return.
 func (robot *Robot) Name() (string, error) {
+	// If name is already set, return it
+	if len(robot.name) == 5 {
+		return robot.name, nil
+	}
+	// Otherwise, generate and set it on the Robot.
 	robot.name = GenerateName()
 
 	return robot.name, nil
 }
 
+// Reset a robot's name to something unique.
 func (robot *Robot) Reset() (bool, error) {
 	robot.name = GenerateName()
 
 	return true, nil
 }
 
+// Generate a robot name. If already used, continue trying until a unique one is found.
 func GenerateName() string {
+	// robotNames might need to be initialized
+	if len(robotNames) <= 0 {
+		robotNames = make(map[string]int)
+	}
+
 	name := GeneratePrefix() + GenerateSuffix()
 	for NameAlreadyUsed(name) {
 		name = GeneratePrefix() + GenerateSuffix()
 	}
+	
+	robotNames[name] = 1
 
 	return name
 }
@@ -43,6 +58,7 @@ func NameAlreadyUsed(name string) bool {
 	return seen
 }
 
+// Generate a random 2-letter prefix.
 func GeneratePrefix() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	char1 := letters[r.Intn(26)]
@@ -51,6 +67,7 @@ func GeneratePrefix() string {
 	return char1 + char2
 }
 
+// Generate a random 3-digit suffix.
 func GenerateSuffix() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	num1 := r.Intn(10)
