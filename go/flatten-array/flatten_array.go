6e062c638ab92d6f6832317749d089df83f98879
knowaveragejoe <25515709+knowaveragejoe@users.noreply.github.com>
2021-07-13 23:02:31 -0400

    Flattened array complete

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,1 +1,31 @@
 package flatten
+
+var flattened []interface{}
+
+func Flatten(input interface{}) []interface{} {
+	flattened := []interface{}{}
+	DoFlatten(&flattened, input)
+
+	return flattened
+}
+
+func DoFlatten(accum *[]interface{}, input interface{}) {
+	// Assert the input slice is actually usable
+	slice := input.([]interface{})
+
+	for _, item := range slice {
+		// Skip nil values
+		if item == nil {
+			continue
+		}
+
+		// If value is an int, append to accumulator
+		switch t := item.(type) {
+		case int:
+			*accum = append(*accum, t)
+		case []interface{}:
+			//Otherwise, recurse
+			DoFlatten(accum, t)
+		}
+	}
+}
