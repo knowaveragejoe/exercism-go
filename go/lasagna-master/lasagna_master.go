e5d55985d3b5d16f6113205dd284642c781922ec
Joseph Clark <joseph@jclark.codes>
2023-01-30 23:35:34 -0500

    more exercises

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,39 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, layerPrepTime int) int {
+	if layerPrepTime <= 0 {
+		layerPrepTime = 2
+	}
+
+	return len(layers) * layerPrepTime
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (noodles int, sauce float64) {
+	for _, v := range layers {
+		if v == "noodles" {
+			noodles += 50
+		} else if v == "sauce" {
+			sauce += 0.2
+		}
+	}
+
+	return noodles, sauce
+}
 
 // TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendList []string, ingredientList []string) {
+	ingredientList[len(ingredientList)-1] = friendList[len(friendList)-1]
+}
 
-// TODO: define the 'ScaleRecipe()' function
+// TODO: define the 'ScaleRecipe()'
+func ScaleRecipe(amounts []float64, portions int) []float64 {
+	scaled := make([]float64, len(amounts))
+
+	for i, v := range amounts {
+		scaled[i] = (v * float64(portions)) / 2
+	}
+
+	return scaled
+}
