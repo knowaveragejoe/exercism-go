4124e97ad62784655690abc7b1cdd23710ef4234
Joseph Clark <joseph@jclark.codes>
2023-02-02 00:51:06 -0500

    finishing logs exercisings

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,35 @@
 package logs
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	// var application string
+	for _, c := range log {
+		char := fmt.Sprintf("%U", c)
+		if char == "U+2757" {
+			return "recommendation"
+		} else if char == "U+1F50D" {
+			return "search"
+		} else if char == "U+2600" {
+			return "weather"
+		}
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return len([]rune(log)) <= limit
 }
